c8d5237f0a4b564242c70e3f722581bb5abd3249
Miguel <miguelbermell@gmail.com>
2022-05-13 18:53:50 +0200

    Add news model

---
+++ src/models/News.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+
+const newsSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    author: {
+      type: String,
+      required: true,
+      maxlength: 100
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    archiveDate: {
+      type: Date,
+      default: null
+    },
+    image: {
+      type: String,
+      default: null
+    }
+  },
+  { timestamps: { createdAt: 'date', updatedAt: 'updatedAt' } }
+)
+
+newsSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const News = model('News', newsSchema)
+
+module.exports = News
